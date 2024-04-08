68017a058fc6ffafaf921d383df7db087c3ce053
moriokadaiki <daiki.morioka.w285@dirbato.co.jp>
2024-04-08 14:45:56 +0900

    コミット

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,14 +1,16 @@
 package company;
 
+import java.util.spi.AbstractResourceBundleProvider;
+
 public class Employee {
     
-    String name;
-    String department;
-    String position;
-    int employeeId;
+    protected final String name;
+    private final Department department;
+    private final String position;
+    private final int employeeId;
 
     
-    public Employee(String name, String department, String position, int employeeId) {
+    public Employee(String name, Department department, String position, int employeeId) {
         this.name = name;
         this.department = department;
         this.position = position;
@@ -23,6 +25,11 @@ public class Employee {
     
     public void report() {
         report(1);
+    }
+    
+    public void joinMeeting() {
+        department.meeting();
+        System.out.println("→上記の会議に参加します。部署："+ department.getName()+"名前："+ name);
     }
 }
 
--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -4,17 +4,22 @@ public class ObjectSample {
 
     public static void main(String[] args) {
         
-        var employee = new Employee("鈴木", "営業部", "課長", 100);
+        var department =new Department("営業部", "ｘｘ", 1000000);
+        var employee = new Employee("鈴木", department, "課長", 100);
 
         
         employee.report();
         employee.report(2);
+        employee.joinMeeting();
         
         System.out.println("");
         
-        var engineer = new Employee("田中", "開発部", "一般社員", 88);
+        var devDepartment = new Department("開発部", "yy", 0);
+        var engineer = new Employee("田中", devDepartment, "一般社員", 88);
         
         engineer.report();
+        engineer.joinMeeting();
+        
     }
 
 }
