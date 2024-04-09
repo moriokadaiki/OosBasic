1ef4dfd9ea2b7ac1e4a7a11c332ce6c204435a93
moriokadaiki <daiki.morioka.w285@dirbato.co.jp>
2024-04-09 11:56:18 +0900

    20240409

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,15 +1,13 @@
 package company;
 
-import java.util.spi.AbstractResourceBundleProvider;
-
 public class Employee {
-    
+    // フィールド
     protected final String name;
     private final Department department;
     private final String position;
     private final int employeeId;
 
-    
+    // コンストラクター
     public Employee(String name, Department department, String position, int employeeId) {
         this.name = name;
         this.department = department;
@@ -17,19 +15,19 @@ public class Employee {
         this.employeeId = employeeId;
     }
 
-    
+    // 報告メソッド
     public void report(int times) {
         System.out.println(times + "回目の報告をします。役職：" + position + "、名前：" + name);
     }
 
-    
+    // 報告メソッド（オーバーロード）
     public void report() {
         report(1);
     }
     
+    // 会議に参加するメソッド
     public void joinMeeting() {
         department.meeting();
-        System.out.println("→上記の会議に参加します。部署："+ department.getName()+"名前："+ name);
+        System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前：" + name);
     }
 }
-
--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -3,23 +3,55 @@ package company;
 public class ObjectSample {
 
     public static void main(String[] args) {
-        
-        var department =new Department("営業部", "ｘｘ", 1000000);
+        // インスタンスの作成
+        var department = new Department("営業部", "xx", 1000000);
         var employee = new Employee("鈴木", department, "課長", 100);
 
-        
+        // インスタンスメソッドの呼び出し
         employee.report();
         employee.report(2);
         employee.joinMeeting();
-        
+
+        // 1行空ける
         System.out.println("");
-        
+
+        // インスタンスの作成
         var devDepartment = new Department("開発部", "yy", 0);
-        var engineer = new Employee("田中", devDepartment, "一般社員", 88);
-        
+        var engineer = new Engineer("田中", devDepartment, "一般社員", 88, "Java");
+
+        // インスタンスメソッドの呼び出し
         engineer.report();
         engineer.joinMeeting();
+        engineer.developSoftware();
+
+        // 1行空ける
+        System.out.println("");
+
+        // ポリモーフィズムの確認
+        Employee projectManager = new Engineer("佐藤", devDepartment, "PM", 99, "Java");
+
+        // インスタンスメソッドの呼び出し
+        projectManager.report();
+        projectManager.joinMeeting();
+        if (projectManager instanceof Engineer) {
+            // developSoftwareメソッドを呼び出す
+            ((Engineer) projectManager).developSoftware();
+        }
+
+        // ----- 追記（ここから） -----
+        // 1行空ける
+        System.out.println("");
+
+        // アルバイトインスタンスの作成
+        var parttimeWorker = new ParttimeWorker("太田", department);
         
+        // 実装したメソッドの呼び出し
+        ((Workable) employee).work();
+        ((Workable) engineer).work();
+        ((Workable) projectManager).work();
+        ((Workable) parttimeWorker).work();
+        // ----- 追記（ここまで） -----
+
     }
 
 }
